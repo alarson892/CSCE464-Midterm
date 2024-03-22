91cda31f8b52d9a8f5b4f8ed086c5cf96b3ccb02
alarson892 <48035438+alarson892@users.noreply.github.com>
2024-03-22 01:16:34 -0500

    Lots of stuff
    Added joke button, changed formatting and content pretty much entirely.

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,13 @@
-//test
+const joke = async () => {
+    const response = await fetch('https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,racist,sexist,explicit');
+    const responseJson = await response.json();
+    var jokeString;
+    if(responseJson.type=='single'){
+        jokeString = responseJson.joke;
+    }else{
+        jokeString = responseJson.setup + "\n \n"+ responseJson.delivery;
+    }
+    alert(jokeString);
+  }
+
 
